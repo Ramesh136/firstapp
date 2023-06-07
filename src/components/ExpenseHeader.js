2c51d05a89cde8d4d9096b31f20d1ea5cef6b32c
ramesh <rameshricky24@gmail.com>
2023-06-07 15:17:59 +0530

    first commit

---
+++ src/components/ExpenseHeader.js
@@ -0,0 +1,9 @@
+
+function ExpenseHeader(){
+
+    return (
+        <h1>Expenses</h1>
+    )
+}
+
+export default ExpenseHeader
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,14 @@
+
+function ExpenseList(){
+
+    return (
+        <ul>
+            <li>Rs 100</li>
+            <li>Rs 800</li>
+            <li>Rs 500</li>
+            <li>Rs 300</li>
+        </ul>
+    )
+}
+
+export default ExpenseList
