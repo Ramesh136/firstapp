75a1c032c93b4b7066565faea44b68d41a2d5a38
ramesh <rameshricky24@gmail.com>
2023-08-04 01:48:41 +0530

    Smaller components

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h1>Expenses</h1>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
