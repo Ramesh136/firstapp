1095a9da84f3ca4af3a9512000fe89b89ab11bfa
ramesh <rameshricky24@gmail.com>
2023-08-18 01:34:59 +0530

    deleteing price

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,11 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import { useState } from 'react'
 
 
 function ExpenseItem(props){
+
+    const [price , setPrice] = useState(props.amount)
 
     return (
       <div className='expense-item'>
@@ -10,8 +13,13 @@ function ExpenseItem(props){
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <h2>{props.location}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>${price}</div>
       </div>
+      <button
+        onClick = {()=>{
+          setPrice(0)
+        }}
+      >delete</button>
     </div>
     )
 }
